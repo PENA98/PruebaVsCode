ea2708d97de50bbcbe9f0e42178c7b78d72eef6d
Maynor Peña <38294810+PENA98@users.noreply.github.com>
2019-06-11 23:36:26 -0600

    Primer commit de prueba

---
+++ js/jscode.js
@@ -0,0 +1,7 @@
+function changeStyle(){
+
+    var text = document.getElementById("para1").style.color = "red";
+    var text = document.getElementById("para1").style.backgroundColor = "cyan";
+    var text = document.getElementById("para1").style.fontSize = "5em";
+
+}
